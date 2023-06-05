c8703009938012890e046feb365948dc93e25b79
Maykal Tenev <michaeltenev@gmail.com>
2023-06-05 23:46:27 +0200

    angular components

--- angular-basics/src/app/app.module.ts
+++ angular-basics/src/app/app.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './Container/container.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
+import { NotificationComponent } from './notification/notification.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ContainerComponent, NavComponent, HeaderComponent, NotificationComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
